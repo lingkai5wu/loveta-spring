4ad5c8f6c3a16bdafa7d7658da0b10ee58c864a3
lingkai5wu <lingkai5wu@outlook.com>
2023-12-25 11:08:02 +0800

    chore: 初始化项目

---
+++ src/main/java/com/github/lingkai5wu/loveta/LoveTaApplication.java
@@ -0,0 +1,13 @@
+package com.github.lingkai5wu.loveta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveTaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoveTaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/lingkai5wu/loveta/LoveTaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lingkai5wu.loveta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveTaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
