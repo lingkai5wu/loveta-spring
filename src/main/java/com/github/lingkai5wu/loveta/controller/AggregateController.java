22cfee5da3dbf6f43480f341274406e2dfa8466a
lingkai5wu <lingkai5wu@outlook.com>
2024-03-03 19:07:32 +0800

    refactor: 重命名获取运行时数据接口

--- src/main/java/com/github/lingkai5wu/loveta/controller/AggregateController.java
+++ src/main/java/com/github/lingkai5wu/loveta/controller/AggregateController.java
@@ -4,7 +4,7 @@ import cn.dev33.satoken.stp.StpUtil;
 import cn.hutool.core.bean.BeanUtil;
 import com.github.lingkai5wu.loveta.model.Result;
 import com.github.lingkai5wu.loveta.model.vo.MenuVO;
-import com.github.lingkai5wu.loveta.model.vo.aggregate.RuntimeVO;
+import com.github.lingkai5wu.loveta.model.vo.aggregate.RuntimeDataVO;
 import com.github.lingkai5wu.loveta.service.IMenuService;
 import com.github.lingkai5wu.loveta.service.IUserService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,12 +28,12 @@ public class AggregateController {
     /**
      * 获取运行时数据
      */
-    @GetMapping("/runtime")
-    public Result<RuntimeVO> getRuntimeVO() {
+    @GetMapping("/runtime-data")
+    public Result<RuntimeDataVO> getRuntimeDataVO() {
         long id = StpUtil.getLoginIdAsLong();
-        RuntimeVO runtimeVO = new RuntimeVO()
+        RuntimeDataVO dataVO = new RuntimeDataVO()
                 .setUserVO(userService.getUserVOById(id))
                 .setMenuVOList(BeanUtil.copyToList(menuService.listMenusByUserId(id), MenuVO.class));
-        return Result.data(runtimeVO);
+        return Result.data(dataVO);
     }
 }
--- src/main/java/com/github/lingkai5wu/loveta/model/vo/aggregate/RuntimeDataVO.java
+++ src/main/java/com/github/lingkai5wu/loveta/model/vo/aggregate/RuntimeDataVO.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @Data
 @Accessors(chain = true)
-public class RuntimeVO {
+public class RuntimeDataVO {
     /**
      * 当前用户
      */
